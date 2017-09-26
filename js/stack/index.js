13e3f18bc0fdd13a2132ec66d980174e39b7f437
AliOgretmen <aliogretmen.ch@gmail.com>
2017-09-26 08:03:13 +0200

    second commit (full answered)

--- js/stack/index.js
+++ js/stack/index.js
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * Add new item into the stack
  * @param  pItem, item to be added
@@ -12,49 +13,51 @@ Array.prototype.size = function (pItem) {
  * Remove the top of the stack from the stack
  */
 Array.prototype.remove = function () {
-        this.pop();
-    }
-    /**
-     * Shos the item inthe top of the stack
-     */
+    this.pop();
+};
+/**
+ * Shos the item inthe top of the stack
+ */
 Array.prototype.peek = function () {
-        return this[this.length - 1];
-    }
-    /**
-     * Delelte all items from the stack
-     */
+    return this[this.length - 1];
+};
+/**
+ * Delelte all items from the stack
+ */
 Array.prototype.reset = function () {
     this.length = 0;
-}
-    /**
-     * Ask for the emptyness
-     */
+};
+/**
+ * Ask for the emptyness
+ */
 Array.prototype.isEmpty = function () {
-        if (this.length > 0) {
-            return false;
-        }
+    if (this.length > 0) {
+        return false;
+    }
         else {
             return true;
         }
-    }
-    /**
-     * Creates a stack from a given array
-     * stack.$stackOf([7,8,9,11]]) 
-     */
+};
+/**
+ * Creates a stack from a given array
+ * stack.$stackOf([7,8,9,11]]) 
+ */
 Array.prototype.$stackOf = function (pArray) {
-    
     this.reset();
-    for (let i=0; i<pArray.length; i++){
-    this.push(pArray[i]);
+    for (let i = 0; i < pArray.length; i++) {
+        this.push(pArray[i]);
     }
-    console.log(this);
-    
-}
-    /**
-     * Creates a stack from a the given parameters
-     * stack._stackOf(1,2,3,5)
-     * Please do not change the function below!!!
-     */
-Array.prototype._stackOf = function () {
-    
-}
+    // for (let value of pArray){
+    //this.add(value);
+    //  };
+};
+/**
+ * Creates a stack from a the given parameters
+ * stack._stackOf(1,2,3,5)
+ * Please do not change the function below!!!
+ */
+Array.prototype._stackOf = function (...items) {
+        for (let value of items) {
+            this.add(value);
+        }
+};
